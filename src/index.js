b8e04c3e0e0eff8a7196c966b084ccc5f51de1b7
aslisonas <alison8244@protonmail.com>
2024-08-08 14:28:00 -0300

    remover dependencias inutilizadas

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import puppeteer from "puppeteer";
 const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
 
-import { select, Separator } from "@inquirer/prompts";
-
 navigateLogin(page);
 navigateMainMenu(page);
