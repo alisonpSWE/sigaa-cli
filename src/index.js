fb3fb19e0366ec9f7b055196b0b2bf5782ae235f
aslisonas <alison8244@protonmail.com>
2024-08-15 21:50:28 -0300

    Pequenos ajustes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import { navigateLogin, navigateMainMenu } from "./web.js";
 console.log("at index.js");
 import puppeteer from "puppeteer";
 
-const browser = await puppeteer.launch({ headless: false });
+const browser = await puppeteer.launch({
+  headless: false,
+  devtools: false,
+  slowMo: 0,
+  timeout: 30000,
+  args: ["--enable-logging,", "--v=1"],
+});
 const page = await browser.newPage();
 
 navigateLogin(page);
--- src/menus/portalDiscente.js
+++ src/menus/portalDiscente.js
@@ -27,7 +27,6 @@ export async function navigatePortalDiscente(page) {
 }
 
 function extractLinkId(evalHTML, valorAlvo) {
-  // Regex to find the block that contains input and link
   const regex = new RegExp(
     `<input[^>]*type="hidden"[^>]*value="${valorAlvo}"[^>]*name="idTurma"[^>]*>.*?<a[^>]*id="([^"]+)"[^>]*>`,
     "s"
@@ -47,25 +46,20 @@ function extractDataTurmasPortal(html) {
   let result = [];
   let match;
 
-  // Iterate over all table rows
   while ((match = trRegex.exec(html)) !== null) {
     const trContent = match[1];
 
-    // Extract name
     const nameMatch = nameRegex.exec(trContent);
     const name = nameMatch ? nameMatch[1].trim() : null;
 
-    // Extract value
     const valueMatch = valueRegex.exec(trContent);
     const value = valueMatch ? valueMatch[1] : null;
 
-    // Extract dataString
     const dataStringMatch = dataStringRegex.exec(trContent);
     const dataString = dataStringMatch
       ? dataStringMatch[1].replace(/<br>/g, "").replace(/\s+/g, " ").trim()
       : null;
 
-    // Add to result if all parts are present
     if (name && value && dataString) {
       result.push({ name, value, dataString });
     }
