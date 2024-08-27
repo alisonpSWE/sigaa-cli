5977aed3248d386fc96d3ea740bfaa436c0233ba
aslisonas <alison8244@protonmail.com>
2024-08-26 23:26:55 -0300

    fix: Organizado para ser mais modular

--- src/menus/menuTurma.js
+++ src/menus/menuTurma.js
@@ -1,13 +1,31 @@
-import { extrairConteudoTurmas } from "../utils/stringManipulationUtil.js";
+import {
+  extrairConteudoParagrafos,
+  extrairConteudoTurmas,
+  tempoAtras,
+} from "../utils/stringManipulationUtil.js";
 import { select, Separator } from "@inquirer/prompts";
 import chalk from "chalk";
 
+let atualConteudosHTMLString;
+
 export async function navegarMenuDeTurmas(page) {
+  atualConteudosHTMLString = await ConteudosHTMLString(page);
   const opcaoDeNavegacao = await mostrarMenuPrincipal();
 
   if (opcaoDeNavegacao === "escolherMaterial") {
-    await escolhaMaterial(page);
+    await escolhaMaterial();
   }
+}
+
+async function ConteudosHTMLString(page) {
+  await page.waitForSelector("#conteudo");
+  const elementoConteudo = await page.$("#conteudo");
+
+  if (!elementoConteudo) {
+    throw new Error("Elemento #conteudo não encontrado");
+  }
+
+  return await elementoConteudo.evaluate((elemento) => elemento.innerHTML);
 }
 
 async function mostrarMenuPrincipal() {
@@ -19,19 +37,8 @@ async function mostrarMenuPrincipal() {
   });
 }
 
-async function escolhaMaterial(page) {
-  await page.waitForSelector("#conteudo");
-  const elementoConteudo = await page.$("#conteudo");
-
-  if (!elementoConteudo) {
-    throw new Error("Elemento #conteudo não encontrado");
-  }
-
-  const conteudoHTML = await elementoConteudo.evaluate(
-    (elemento) => elemento.innerHTML
-  );
-  const turmasConteudo = extrairConteudoTurmas(conteudoHTML);
-
+async function escolhaMaterial() {
+  const turmasConteudo = extrairConteudoTurmas(atualConteudosHTMLString);
   const choices = turmasConteudo.reduce((conteudos, turma) => {
     conteudos.push(new Separator(chalk.yellowBright(turma.titulo)));
     turma.itens.forEach((item) => {
