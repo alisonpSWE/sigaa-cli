c1cef1566e804a257c63242adf133b0e9628eae0
aslisonas <alison8244@protonmail.com>
2024-08-26 22:04:17 -0300

    feat: Pular telas de avisos em modo desenvolvimento

--- src/utils/telasAviso.js
+++ src/utils/telasAviso.js
@@ -1,6 +1,14 @@
 import { confirm } from "@inquirer/prompts";
 
+if (process.env.NODE_ENV === "development") {
+  const isDevMode = true;
+}
+
 export async function manuseiarTelaAvisoLogon(page) {
+  if (isDevMode) {
+    page.click('[value="Continuar >>"]');
+  }
+
   console.log("Mensagem oficial de aviso em: " + page.url());
   const continuar = await confirm({ message: "Continuar a navegação?" });
   if (continuar) {
