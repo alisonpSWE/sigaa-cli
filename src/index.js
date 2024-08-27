c4909be128a038394ea067a70f6b0ff29abc2b16
aslisonas <alison8244@protonmail.com>
2024-08-26 21:36:58 -0300

    fix: Corrigido mostrar opções de debug apenas em desenvolvimento

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import { navigateLogin, navigateMainMenu } from "./web.js";
 import puppeteer from "puppeteer";
 
-const browser = await puppeteer.launch({
-  headless: false,
-  devtools: false,
-  slowMo: 0,
-  timeout: 30000,
-  args: ["--enable-logging,", "--v=1"],
-});
+let launchOptions = {};
+
+if (process.env.NODE_ENV === "development") {
+  launchOptions = {
+    headless: false,
+    devtools: false,
+    slowMo: 0,
+    timeout: 30000,
+    args: ["--enable-logging", "--v=1"],
+  };
+}
+
+const browser = await puppeteer.launch(launchOptions);
+
 const page = await browser.newPage();
 
 navigateLogin(page);
