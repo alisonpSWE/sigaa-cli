18f6e1b7840d381d029ef3f8c9f07a74f159b875
aslisonas <alison8244@protonmail.com>
2024-09-14 13:53:53 -0300

    fix: Corrigido manueseio de tela aviso Logon ao encontrar multiplas telas

--- src/utils/telasAviso.js
+++ src/utils/telasAviso.js
@@ -1,12 +1,18 @@
 import { confirm } from "@inquirer/prompts";
-
-const isDevMode = process.env.NODE_ENV === "development";
+import { esperarCarregamentoTotalHtml } from "./utils.js";
 
 export async function manuseiarTelaAvisoLogon(page) {
-  if (isDevMode) {
-    return page.click('[value="Continuar >>"]');
-  }
+  if (await page.url().endsWith("telaAvisoLogon.jsf")) {
+    await page.click('[value="Continuar >>"]');
+    await esperarCarregamentoTotalHtml(page);
 
+    if (await page.url().endsWith("telaAvisoLogon.jsf")) {
+      await manuseiarTelaAvisoLogon(page);
+    }
+  }
+}
+
+async function mensagemConfirmacao(page) {
   console.log("Mensagem oficial de aviso em: " + page.url());
   const continuar = await confirm({ message: "Continuar a navegação?" });
   if (continuar) {
